5a743d7f4221befe8cef6b034afb1c029b606a8d
meyerlf <meyerlf@iu.edu>
2024-04-18 00:05:32 -0400

    test

--- driver.cpp
+++ driver.cpp
@@ -195,14 +195,14 @@ int main(int argc, char* argv[])
 	Tree_Node * n1 = new Number_Node(4);
 	Tree_Node * n2 = new Number_Node(2);
 	Add_Node * n3 = new Add_Node(n1, n2);
-	//Tree_Node * n4 = new Subtract_Node(new Number_Node(4), new Number_Node(2));
-	//Tree_Node * n5 = new Multiply_Node(new Number_Node(4), new Number_Node(2));
-	//Tree_Node * n6 = new Divide_Node(new Number_Node(4), new Number_Node(2));
-	//Tree_Node * n7 = new Modulo_Node(new Number_Node(4), new Number_Node(3));
+	Tree_Node * n4 = new Subtract_Node(new Number_Node(4), new Number_Node(2));
+	Tree_Node * n5 = new Multiply_Node(new Number_Node(4), new Number_Node(2));
+	Tree_Node * n6 = new Divide_Node(new Number_Node(4), new Number_Node(2));
+	Tree_Node * n7 = new Modulo_Node(new Number_Node(4), new Number_Node(3));
 
 	delete n3;
-	//delete n4;
-	//delete n5;
-	//delete n6;
-	//delete n7;
+	delete n4;
+	delete n5;
+	delete n6;
+	delete n7;
 }
