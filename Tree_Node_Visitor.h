cebfd29153a7e66a27faf6b336fcce61e9602050
meyerlf <meyerlf@iu.edu>
2024-04-17 22:03:16 -0400

    make methods virtual

--- Tree_Node_Visitor.h
+++ Tree_Node_Visitor.h
@@ -22,22 +22,22 @@ public:
 		//Methods for visiting Nodes
 	
 	// Visit Number Node
-	void Visit_Number_Node(const Number_Node & node) = 0;
+	virtual void Visit_Number_Node(const Number_Node & node) = 0;
 	
 	// Visit Add Node
-	void Visit_Add_Node(const Add_Node & node) = 0;
+	virtual void Visit_Add_Node(const Add_Node & node) = 0;
 	
 	// Visit Subtract Node
-	void Visit_Subtract_Node(const Subtract_Node & node) = 0;
+	virtual void Visit_Subtract_Node(const Subtract_Node & node) = 0;
 	
 	// Visit Multiply Node
-	void Visit_Multiply_Node(const Multiply_Node & node) = 0;
+	virtual void Visit_Multiply_Node(const Multiply_Node & node) = 0;
 
 	// Visit Divide Node
-	void Visit_Divide_Node(const Divide_Node & node) = 0;
+	virtual void Visit_Divide_Node(const Divide_Node & node) = 0;
 
 	// Visit Modulo Node
-	void Visit_Modulo_Node(const Modulo_Node & node) = 0;
+	virtual void Visit_Modulo_Node(const Modulo_Node & node) = 0;
 };
 
 #endif   // !defined _TREE_NODE_VISITOR_H_
