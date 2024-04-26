14be3e0a53f672476104318d2c90e8949fc5ea11
meyerlf <meyerlf@iu.edu>
2024-04-26 13:17:40 -0400

    test

--- driver.cpp
+++ driver.cpp
@@ -112,7 +112,7 @@ int main(int argc, char* argv[])
 	std::string infix;
 	Stack<int> result = Stack<int>();
 	Postfix_Builder build(result);
-	Postfix_Expr postfix(result);
+	Postfix_Expr * postfix(result);
 
 	while (true) {
 		// get user input into 'infix'
