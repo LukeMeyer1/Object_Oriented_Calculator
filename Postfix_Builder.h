614eaf982d5bd1212deaa2ff4b21e02e2a6180de
meyerlf <meyerlf@iu.edu>
2024-04-30 15:57:59 -0400

    test

--- Postfix_Builder.h
+++ Postfix_Builder.h
@@ -51,7 +51,7 @@ public:
 	*
 	* @retval	Expression_Parser&		postfix parser that will work with the builder
 	*/
-	Postfix_Parser * parser(void);
+	Expression_Parser * parser(void);
 
 private:
 	// helper function to add operator onto temp stack
